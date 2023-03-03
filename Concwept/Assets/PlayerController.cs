ea2997baa1992625040427f39dd5233dcf5246d6
igsolis <112159393+igsolis@users.noreply.github.com>
2023-03-02 19:00:47 -0800

    ddddd
    d

--- Concwept/Assets/PlayerController.cs
+++ Concwept/Assets/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Vector2 move;
 
+
     public void OnMove(InputAction.CallbackContext context) 
     {
         move = context.ReadValue<Vector2>();
@@ -24,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         movePlayer();
     }
+    
     public void movePlayer()
     {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
